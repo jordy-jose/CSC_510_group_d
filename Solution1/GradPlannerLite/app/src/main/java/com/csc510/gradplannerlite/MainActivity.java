a5a1313c6a273f9f4c67ec3bfe9bf78d94327bea
Jordy Jose <jordy.jos@gmail.com>
2016-02-25 01:08:50 -0500

    created an activity for handling user information

--- Solution1/GradPlannerLite/app/src/main/java/com/csc510/gradplannerlite/MainActivity.java
+++ Solution1/GradPlannerLite/app/src/main/java/com/csc510/gradplannerlite/MainActivity.java
@@ -1,5 +1,6 @@
 package com.csc510.gradplannerlite;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -48,5 +50,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onClickUpdateInfo(View view) {
+        Intent intent = new Intent(this, UpdateInfo.class);
+        startActivity(intent);
     }
 }
